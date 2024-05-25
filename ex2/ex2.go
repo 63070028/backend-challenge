aa37b517594be8419403089f9a46c210a8d00641
chanapon chuenchuwit <63070028@kmitl.ac.th>
2024-05-25 14:34:30 +0700

    ex2

--- ex2/ex2.go
+++ ex2/ex2.go
@@ -1,70 +1,118 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	fmt.Println(decodePassword("LLRR=")) //should 210122
 	fmt.Println(decodePassword("==RLL")) //should 000210
 	fmt.Println(decodePassword("=LLRR")) //should 221012
 	fmt.Println(decodePassword("RRL=R")) //should 012001
+
+	fmt.Println(decodePassword("RRR"))   //should 0123
+	fmt.Println(decodePassword("LLL"))   //should 3210
+	fmt.Println(decodePassword("LLL=L")) //should 432110
+	fmt.Println(decodePassword(""))      //should 0
+
 }
 
 func decodePassword(s string) string {
-	// ans := ""
-	// password := [len(s)+1]int
-	// for i := 0; i<len(s); i++{
-	// 	if(s[i] == 'L'){
-			
-	// 	}
-	// }
 
-	return ""
+	pass := make([]int, len(s)+1)
+	for i, v := range s {
+		if v == '=' {
+			pass[i+1] = pass[i]
+		} else if v == 'L' {
+			if pass[i] == 1 {
+				pass[i+1] = 0
+			} else {
+				pass[i+1] = -1
+			}
+		} else if v == 'R' {
+			pass[i+1] = pass[i] + 1
+		}
+
+		if pass[i+1] < 0 {
+			pass[i+1]++
+			for j := i; j >= 0; j-- {
+				if s[j] == 'L' {
+					if pass[j] > pass[j+1] {
+						break
+					} else {
+						pass[j]++
+					}
+				} else if s[j] == 'R' {
+					if pass[j] < pass[j+1] {
+						break
+					}
+				} else if s[j] == '=' {
+					if pass[j] == pass[j+1] {
+						break
+					} else {
+						pass[j] = pass[j+1]
+					}
+				}
+
+			}
+		}
+	}
+
+	reuslt := ""
+	for _, v := range pass {
+		reuslt += strconv.Itoa(v)
+	}
+
+	return reuslt
 }
 
+/*
 
-//R
-//0,1
+	= = R L L
 
-//RR
-//0,1,2
+	[]
+	= -> 0,0
 
-//RRR
-//0,1,2,3
+	[0,0]
+	= -> 0,0
 
+	[0,0,0]
+	R -> 0,1
 
-//L
-//1,0
+	[0,0,0,1]
+	L -> 1,0
 
-//LL
-//2,1,0
+	[0,0,0,1,0]
+	L -> 0,-1
 
-//LR
-//L -> p[0] = p[1]+1
-//R -> p[1] = p[2]-1
-//p[2] 0
-// 0 -> -1 ->0
-// 1 0 1
+	[0,0,0,1,0,-1]
+	last < 0 -> all(pass) +1
+	[1,1,1,2,1,0] incorrect
+	-1 + 1 = 0
 
-//LRR
-//L -> p[0] = p[1]+1
-//R -> p[1] = p[2]-1
-//R -> p[2] = p[3]-1
-//p[3] 0
-// 0 -> -1 -> -2 -> -1
-// 2, 1, 0, 1
-// 1 0 1 2
+	(L) 0 > 0 ? -> No 0+1 -> 1
+	(L) 1 > 1 ? -> No 1+1 -> 2
+	(R) 0 < 2 ? -> Yes break
 
-//=R
-//001
+	[0,0,0,2,1,0]
 
-//R=
-//011
+*/
 
-//==RL
-//0 -> 1 -> 0 -> 0 -> 0
-//00010
+/*
 
-//==RR
-//0 -> -1 -> -2 -> -2 -> -2
-//00012
+	R R L = R
+	pass[0,0,0,0,0,0]
 
+	[]
+	R -> 0,1
+
+	[0,1]
+	R -> 1,2
+
+	[0,1,2]
+	L -> 2,1
+
+	[0,1,2,1] incorrect should -> [0,1,2,0]
+
+*/
